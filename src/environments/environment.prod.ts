ed7f1cdd18751485c11bb1c70fc5bf7c97971ef9
Himanshu Kushwaha <himkushwaha08@gmail.com>
2020-06-10 19:41:52 +0530

    fixed some errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA4W5RsaKdm3Qqn_sPpZOo2nN3ngH6nWls',
+    authDomain: 'e-events-3964d.firebaseapp.com',
+    databaseURL: 'https://e-events-3964d.firebaseio.com',
+    projectId: 'e-events-3964d',
+    storageBucket: 'e-events-3964d.appspot.com',
+    messagingSenderId: '1065785724996',
+    appId: '1:1065785724996:web:83f8aae74cf4176dbbec11',
+    measurementId: 'G-VH90T7LKE5'
+  }
 };
